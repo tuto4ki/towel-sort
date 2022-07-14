2cb8353174788e69023294cf7b578b496834e06a
Svetlana.Safonova <safonova.sv.a@gmail.com>
2022-07-14 16:48:54 +0300

    feat: add functional

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix) return [];
+  for(let i = 0; i < matrix.length; i++) {
+    if(i % 2)
+      matrix[i].sort((a, b) => b - a);
+    else
+      matrix[i].sort((a, b) => a - b);
+  }
+  return [].concat(...matrix);
 }
